2a349c5ede4bc725935c51d6432002ebe4ba4e37
Alperen Alperen <aalperen@icloud.com>
2024-08-17 18:23:32 +0300

    backend progress

--- server/main.py
+++ server/main.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from sqlalchemy import create_engine
+from sqlalchemy import TEXT, VARCHAR, Column, LargeBinary, create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 app = FastAPI()
 DATABASE_URL = "postgresql://postgres:Celcius.227282@localhost:5432/spotify"
@@ -17,12 +18,28 @@ class UserCreate(BaseModel):
     password: str
 
 
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(TEXT, primary_key=True)
+    name = Column(VARCHAR(100))
+    email = Column(VARCHAR(100))
+    password = Column(LargeBinary)
+
+
 @app.post("/signup")
 def signup_user(user: UserCreate):
     # extract the data from incoming request
-    print(user.name)
-    print(user.email)
-    print(user.password)
+    # print(user.name)
+    # print(user.email)
+    # print(user.password)
     # check if the user already exists in the db
+    db.query(User).filter(User.email == user.email).first()
     # add user to the db
     pass
+
+
+Base.metadata.create_all(engine)
