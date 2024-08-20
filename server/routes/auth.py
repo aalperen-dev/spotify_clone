68b4414a969e6d9978cf83965be04a2dd9eacb0b
Alperen Alperen <aalperen@icloud.com>
2024-08-20 18:34:07 +0300

    login api route

--- server/routes/auth.py
+++ server/routes/auth.py
@@ -7,11 +7,12 @@ from sqlalchemy.orm import Session
 from database import get_db
 from models.user_model import User
 from pydantic_schemas.user_create import UserCreate
+from pydantic_schemas.user_login import UserLogin
 
 router = APIRouter()
 
 
-@router.post("/signup")
+@router.post("/signup", status_code=201)
 def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     # extract the data from incoming request
     # print(user.name)
@@ -21,9 +22,9 @@ def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     user_db = db.query(User).filter(User.email == user.email).first()
 
     if user_db:
-        raise HTTPException(400, "Email already registered")
+        raise HTTPException(400, "User with the same email already exists!")
 
-    password_hash = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt(16))
+    password_hash = bcrypt.hashpw(user.password.encode(), bcrypt.gensalt())
 
     user_db = User(
         id=str(uuid.uuid4()),
@@ -35,4 +36,20 @@ def signup_user(user: UserCreate, db: Session = Depends(get_db)):
     db.add(user_db)
     db.commit()
     db.refresh(user_db)
+    return user_db
+
+
+@router.post("/login")
+def login_user(user: UserLogin, db: Session = Depends(get_db)):
+    # check if user with same email already exists
+    user_db = db.query(User).filter(User.email == user.email).first()
+
+    if not user_db:
+        raise HTTPException(400, "User with email does not exists!")
+
+    is_match = bcrypt.checkpw(user.password.encode(), user_db.password)
+
+    if not is_match:
+        raise HTTPException(400, "Incorrect password!")
+
     return user_db
