d5307989ca011e0c925ace8e1c8725feac8dbf3f
Alperen Alperen <aalperen@icloud.com>
2024-09-02 19:47:15 +0300

    bottom nav bar

--- client/lib/features/home/view/pages/home_page.dart
+++ client/lib/features/home/view/pages/home_page.dart
@@ -1,16 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:spotify/core/providers/current_user_notifier.dart';
 
-class HomePage extends ConsumerWidget {
+class HomePage extends ConsumerStatefulWidget {
   const HomePage({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends ConsumerState<HomePage> {
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    final user = ref.watch(currentUserNotifierProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Title'),
       ),
       body: Container(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        onTap: (value) {
+          setState(() {
+            selectedIndex = value;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Image.asset(selectedIndex == 0
+                ? 'assets/images/home_filled.png'
+                : 'assets/images/home_unfilled.png'),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset('assets/images/library.png'),
+            label: 'Library',
+          ),
+        ],
+      ),
     );
   }
 }
